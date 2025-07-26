112cf82bdfaf3397c110da7d72140e0be17e7bf0
James Manon-og <jamesmanonog@gmail.com>
2025-07-26 17:34:50 +0800

    Refactor AuthController: enhance login and registration methods with response handling and cookie management

--- backend/service_side/src/auth/auth.controller.ts
+++ backend/service_side/src/auth/auth.controller.ts
@@ -1,24 +1,87 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  Res,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+
 import { AuthService } from './auth.service';
+import { AuthGuard } from './guards/jwt-auth.guard';
+import { Public } from './decorators/public.decorator';
 import { UserLoginDto } from './dto/user-login.dto';
 import { RegisterDto } from './dto/register.dto';
-import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
   @Public()
-  @Post('login')
   @HttpCode(HttpStatus.OK)
-  async login(@Body() userLoginDto: UserLoginDto) {
-    return this.authService.loginUser(userLoginDto);
+  @Post('login')
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  async signIn(@Body() loginDto: UserLoginDto, @Res() res) {
+    const { access_token } = await this.authService.loginUser(loginDto);
+
+    if (!access_token) {
+      return res.status(HttpStatus.UNAUTHORIZED).send({
+        message: 'Invalid credentials',
+      });
+    }
+
+    res.cookie('token', access_token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      path: '/',
+    });
+
+    return res.send({ message: 'Login successful', auth_token: access_token });
   }
 
   @Public()
-  @Post('register')
   @HttpCode(HttpStatus.CREATED)
-  async register(@Body() registerDto: RegisterDto) {
-    return this.authService.register(registerDto);
+  @Post('register')
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  async register(@Body() registerDto: RegisterDto, @Res() res) {
+    const { access_token } = await this.authService.register(registerDto);
+
+    if (!access_token) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+        message: 'Registration failed',
+      });
+    }
+
+    res.cookie('token', access_token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      path: '/',
+    });
+
+    return res.send({
+      message: 'Registration successful',
+      auth_token: access_token,
+    });
+  }
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('logout')
+  async signOut(@Res() res) {
+    res.clearCookie('token');
+    return res.send({ message: 'Logout successful' });
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
