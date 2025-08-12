b27a36ca6a161ac94a59a275b4751f93190e8ef1
James Manon-og <jamesmanonog@gmail.com>
2025-08-12 12:36:15 +0800

    feat: refactor group member and group modules to include PrismaModule imports

--- backend/service_side/src/group-member/group-member.controller.ts
+++ backend/service_side/src/group-member/group-member.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { GroupMemberService } from './group-member.service';
 import { CreateGroupMemberDto } from './dto/create-group-member.dto';
 import { UpdateGroupMemberDto } from './dto/update-group-member.dto';
@@ -19,16 +27,19 @@ export class GroupMemberController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.groupMemberService.findOne(+id);
+    return this.groupMemberService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGroupMemberDto: UpdateGroupMemberDto) {
-    return this.groupMemberService.update(+id, updateGroupMemberDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateGroupMemberDto: UpdateGroupMemberDto,
+  ) {
+    return this.groupMemberService.update(id, updateGroupMemberDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.groupMemberService.remove(+id);
+    return this.groupMemberService.remove(id);
   }
 }
--- backend/service_side/src/group-member/group-member.module.ts
+++ backend/service_side/src/group-member/group-member.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GroupMemberService } from './group-member.service';
 import { GroupMemberController } from './group-member.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [GroupMemberController],
   providers: [GroupMemberService],
 })
